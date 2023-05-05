e496db6d33b7a15e4f3f794b74c981846b753ef0
sarimkhan208 <sarimkhan7275@gmail.com>
2023-05-05 21:48:55 +0530

    done by sarimkhan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ function App() {
 
 
   return (
-    <div className="App">
-      <Allroutes />
+    <div>
+      <Allroutes/>
     </div>
   );
 
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,8 +1,35 @@
-import { Box, Image,Button} from "@chakra-ui/react";
+import { Box, Image,Button, Heading} from "@chakra-ui/react";
 import ProductSidebar from "../components/ProductSidebar";
 import ProductCard from "../components/ProductCard";
+import { useEffect } from "react";
+import { getProducts } from "../redux/Product/action";
+import {useDispatch, useSelector} from 'react-redux'
+import axios from "axios";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { BASE_URL } from "../baseurl";
 
 export default function ProductPage(){
+
+    const dispatch = useDispatch()
+    const {data,isLoading} = useSelector((state)=>state.productReducer)
+    const [params,setParams] = useSearchParams()
+    const location = useLocation()
+    
+    let obj = {
+        params:{
+            minPrice : params.get("minPrice"),
+            maxPrice : params.get("maxPrice"),
+            type : params.get("type"),
+            minWeight : params.get("minWeight"),
+            maxWeight : params.get("maxWeight")
+        }
+    }
+    
+    useEffect(()=>{
+        dispatch(getProducts(obj))
+    },[location])
+
+
     return <Box>
     <Image src='https://assets.cltstatic.com/images/responsive/DefaultBanner_Desktop.webp' alt='banner' />
     <Box p={'4'} bgGradient='linear(to-r, #F8BBD0, #b3d4fc)' >
@@ -20,9 +47,21 @@ export default function ProductPage(){
         </Box>
 
         {/* RIGHT PRODUCT PAGE */}
-        <Box p={'3'} display={'grid'} gridTemplateColumns={'repeat(3,1fr)'} gap={'10px'} >
+        <Box>
             {
-                data.map((el)=>(<ProductCard/>))
+                isLoading?
+                <Heading mt={'5'} fontSize={'60px'} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' >
+                    Loding....
+                </Heading>:
+                <Box>
+                {
+                    data.length==0?<Heading textAlign={'center'} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' mt={'155'} fontSize={'40px'} >No Products available of this type</Heading>:<Box p={'3'} display={'grid'} gridTemplateColumns={'repeat(3,1fr)'} gap={'10px'}>
+                        {
+                            data?.map((el)=>(<ProductCard key={el._id} {...el} />))
+                        }
+                        </Box>
+                }
+                </Box>
             }
         </Box>
 
@@ -31,5 +70,3 @@ export default function ProductPage(){
     </Box>
 } 
 
-
-const data = [1,2,3,4,5,6,8,9]
