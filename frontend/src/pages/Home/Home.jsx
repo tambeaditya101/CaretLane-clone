ea4bb72ff112b06e15b5490a1fd40786d75ee9ee
tambeaditya101 <tambeaditya101@gmail.com>
2023-05-07 01:59:29 +0530

    added nav&foot to home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
 import Carousels from "./Carousels/Carousels";
-import picA from '../../Assests/picA.png'
-import picB from '../../Assests/picB.png'
-import picC from '../../Assests/picC.png'
-import picD from '../../Assests/picD.png'
-import SliderCard from '../../components/sliderCard'
+import picA from "../../Assests/picA.png";
+import picB from "../../Assests/picB.png";
+import picC from "../../Assests/picC.png";
+import picD from "../../Assests/picD.png";
+import SliderCard from "../../components/sliderCard";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 let data__home__img3 = [
   "https://cdn.caratlane.com/media/static/images/V4/2023/CL/04-APR/HP-Banner/6Tiles/WEBp/Desktop_party.webp",
   "https://cdn.caratlane.com/media/static/images/V4/2023/CL/04-APR/HP-Banner/6Tiles/WEBp/Desktop_mangalsutra.webp",
@@ -26,6 +28,7 @@ let data__home__img5 = [
 export const Home = () => {
   return (
     <div>
+      <Navbar />
       <div className="carousel">
         <Carousels />
       </div>
@@ -126,10 +129,18 @@ export const Home = () => {
       </div>
       <div className="bot-pic">
         <div>
-          <img src="https://banner.caratlane.com/live-images/4432338e863546609e2d0fed64117351.jpg" alt="" width="80%" />
+          <img
+            src="https://banner.caratlane.com/live-images/4432338e863546609e2d0fed64117351.jpg"
+            alt=""
+            width="80%"
+          />
         </div>
         <div>
-          <img src="https://banner.caratlane.com/live-images/bdd989ae1f2549ae9f8e8d41c144221e.jpg" alt="" width="80%" />
+          <img
+            src="https://banner.caratlane.com/live-images/bdd989ae1f2549ae9f8e8d41c144221e.jpg"
+            alt=""
+            width="80%"
+          />
         </div>
       </div>
 
@@ -150,29 +161,77 @@ export const Home = () => {
           </div>
         </div>
       </Link>
-      <div className='deliup'>
-        <div className='delivery'>
-          <div className='picA'><img src={picA} alt='image1' /></div>
-          <div className='picB'><img src={picB} alt='image2' /></div>
-          <div className='picC' ><img src={picC} alt='image3' /></div>
-          <div className='picD'><img src={picD} alt='image4' /></div>
+      <div className="deliup">
+        <div className="delivery">
+          <div className="picA">
+            <img src={picA} alt="image1" />
+          </div>
+          <div className="picB">
+            <img src={picB} alt="image2" />
+          </div>
+          <div className="picC">
+            <img src={picC} alt="image3" />
+          </div>
+          <div className="picD">
+            <img src={picD} alt="image4" />
+          </div>
         </div>
       </div>
       <SliderCard />
-      <h1 style={{ textAlign: 'left', width: '94%', margin: ' 30px auto', fontSize: "24px" }}>Shop Our Instagram</h1>
+      <h1
+        style={{
+          textAlign: "left",
+          width: "94%",
+          margin: " 30px auto",
+          fontSize: "24px",
+        }}
+      >
+        Shop Our Instagram
+      </h1>
       <div className="insta-container">
-
-
-        <div className="insta1" ><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/3.jpg' alt='img1'></img></div>
-        <div className="insta2"><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/2.jpg' alt='img2'></img></div>
-        <div className="insta3"><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/6.jpg' alt='img3'></img></div>
-        <div className="insta4"><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/7.jpg' alt='img4'></img></div>
-        <div className="insta5"><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/4.jpg' alt='img5'></img></div>
-        <div className="insta6" ><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/1.jpg' alt='img6'></img></div>
-        <div className="insta7"><img src='https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/5.jpg' alt='img7'></img></div>
+        <div className="insta1">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/3.jpg"
+            alt="img1"
+          ></img>
+        </div>
+        <div className="insta2">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/2.jpg"
+            alt="img2"
+          ></img>
+        </div>
+        <div className="insta3">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/6.jpg"
+            alt="img3"
+          ></img>
+        </div>
+        <div className="insta4">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/7.jpg"
+            alt="img4"
+          ></img>
+        </div>
+        <div className="insta5">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/4.jpg"
+            alt="img5"
+          ></img>
+        </div>
+        <div className="insta6">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/1.jpg"
+            alt="img6"
+          ></img>
+        </div>
+        <div className="insta7">
+          <img
+            src="https://cdn.caratlane.com/media/static/images/V4/2023/CL/05-MAY/Others/Insta/5.jpg"
+            alt="img7"
+          ></img>
+        </div>
       </div>
-
-
 
       <div class="home__email">
         <div>
@@ -196,6 +255,7 @@ export const Home = () => {
           <p>Know More about CaratLane</p>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
