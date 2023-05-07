7e8f571403bf69c3f1bd16c7799236b42a2894b4
PrashantGIT7388 <yd.pt180@gmail.com>
2023-05-08 00:42:21 +0530

    fw21_0565_day4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Allroutes from "./allroutes/Allroutes";
+import Payment from "./pages/Payment";
 
 
 
 function App() {
   return (
     <div>
-      <Allroutes />
-     
+      {/* <Allroutes /> */}
+     <Payment/>
     </div>
   );
 }
