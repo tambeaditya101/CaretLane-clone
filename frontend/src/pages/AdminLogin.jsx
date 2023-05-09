73099912d82f009d140a4b4d1cd00700b34ee29e
9802HEMENSAN <hmahilange9802@gmail.com>
2023-05-09 13:44:17 +0530

    Admin Login added

---
+++ frontend/src/pages/AdminLogin.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
+export default function AdminLogin() {
+  const [email, setEmail] = useState("admin@gmail.com");
+  const [password, setPass] = useState("admin");
+  const navigate = useNavigate();
+  const handleClick = () => {
+   
+        if (email=="admin@gmail.com" && password=="admin") {
+  
+          Swal.fire("Welcome Admin", "Good Morning !");
+ 
+          navigate("/admin");
+        }  else {
+          Swal.fire(
+            "Error",
+            "Something went wrong. Please try after sometime",
+            "error"
+          );
+        }
+    
+     
+  };
+  return (
+    <>
+      <Navbar />
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        //bg={useColorModeValue("gray.50", "gray.800")}
+        bg={"#f6eff6"}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Dear Login As Admin</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              to enjoy all of our cool <Link color={"blue.400"}>features</Link>{" "}
+              ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            //bg={useColorModeValue("white", "gray.700")}
+            bg={"#65497b"}
+            boxShadow={"lg"}
+            p={8}
+            color={"white"}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="enter your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  value={password}
+                  placeholder="enter your password"
+                  onChange={(e) => setPass(e.target.value)}
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={"blue.200"}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  onClick={handleClick}
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+      <Footer />
+    </>
+  );
+ }
